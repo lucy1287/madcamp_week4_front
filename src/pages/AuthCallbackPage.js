c9436aac71ce7aa3aecde115564c15838f517a80
lucy1287 <lucy1287@naver.com>
2024-07-21 01:16:02 +0900

    Feat: 백엔드로 유저정보 보내는 로직 추가

--- src/pages/AuthCallbackPage.js
+++ src/pages/AuthCallbackPage.js
@@ -2,6 +2,61 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
+// const AuthCallbackPage = () => {
+//     useEffect(() => {
+//         const exchangeCodeForToken = async () => {
+//             const params = new URLSearchParams(window.location.search);
+//             const code = params.get('code');
+//             const clientId = process.env.REACT_APP_JAVASCRIPT_API_KEY; // 카카오 JavaScript 키
+//             const redirectUri = 'http://localhost:3000/auth'; // Redirect URI
+//             let accessToken;
+//
+//             try {
+//                 const response = await axios.post('https://kauth.kakao.com/oauth/token', null, {
+//                     params: {
+//                         grant_type: 'authorization_code',
+//                         client_id: clientId,
+//                         redirect_uri: redirectUri,
+//                         code: code,
+//                     },
+//                     headers: {
+//                         'Content-Type': 'application/x-www-form-urlencoded',
+//                     },
+//                 });
+//
+//                 const { access_token } = response.data;
+//                 accessToken = access_token;
+//             } catch(error){
+//                 console.error('Error during exchanging code for access token:', error.response ? error.response.data : error.message);
+//             };
+//
+//             try {
+//                 // 액세스 토큰을 백엔드 서버로 전송
+//                 const response = await axios.post('http://localhost:5000/auth/kakao', {
+//                     accessToken: accessToken,
+//                 });
+//
+//                 // 백엔드에서 반환된 JWT 토큰 저장
+//                 const { token } = response.data;
+//                 localStorage.setItem('jwtToken', token);
+//
+//                 // 홈 페이지로 리다이렉트
+//                 window.location.href = '/';
+//             } catch (error) {
+//                 console.error('Error during authentication:', error);
+//             }
+//          };
+//
+//         exchangeCodeForToken();
+//     }, []);
+//
+//     return (
+//         <div>
+//             <h1>로그인 중...</h1>
+//         </div>
+//     );
+// };
+
 const AuthCallbackPage = () => {
     useEffect(() => {
         const getAccessToken = async () => {
@@ -9,6 +64,8 @@ const AuthCallbackPage = () => {
             const code = params.get('code');
             const clientId = process.env.REACT_APP_JAVASCRIPT_API_KEY; // 카카오 JavaScript 키
             const redirectUri = 'http://localhost:3000/auth'; // Redirect URI
+            let accessToken;
+            let userInfo;
 
             try {
                 const response = await axios.post('https://kauth.kakao.com/oauth/token', null, {
@@ -23,9 +80,10 @@ const AuthCallbackPage = () => {
                     },
                 });
 
-                const { access_token } = response.data;
-
+                const {access_token} = response.data;
+                accessToken = access_token;
                 console.log(access_token)
+
 
                 // 액세스 토큰을 사용하여 사용자 정보 요청
                 const userInfoResponse = await axios.get('https://kapi.kakao.com/v2/user/me', {
@@ -34,10 +92,28 @@ const AuthCallbackPage = () => {
                     },
                 });
 
-                const userInfo = userInfoResponse.data;
+                userInfo = userInfoResponse.data;
                 console.log(userInfo);
                 // 사용자 정보를 로컬 스토리지에 저장하거나 상태 관리
                 localStorage.setItem('user', JSON.stringify(userInfo));
+
+                // 홈 페이지로 리다이렉트
+                //window.location.href = '/';
+            } catch (error) {
+                console.error('Error during authentication:', error);
+            }
+
+            try {
+                // 액세스 토큰을 백엔드 서버로 전송
+                const response = await axios.post('http://localhost:5000/user/login', {
+                    kakao_id: userInfo.id,
+                    nickname: userInfo.properties.nickname,
+                    photo: userInfo.properties.profile_image
+                });
+
+                // 백엔드에서 반환된 JWT 토큰 저장
+                const { token } = response.data;
+                localStorage.setItem('jwtToken', token);
 
                 // 홈 페이지로 리다이렉트
                 window.location.href = '/';
@@ -57,3 +133,4 @@ const AuthCallbackPage = () => {
 };
 
 export default AuthCallbackPage;
+
