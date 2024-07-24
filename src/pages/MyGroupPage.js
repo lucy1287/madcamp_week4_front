4171590a62233ab63cc2e284b3631660dcec2da7
lucy1287 <lucy1287@naver.com>
2024-07-24 14:01:27 +0900

    Feat: 내 그룹 백엔드 연동

--- src/pages/MyGroupPage.js
+++ src/pages/MyGroupPage.js
@@ -1,15 +1,32 @@
 // src/pages/MyGroupPage.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MyGroupPage.css'; // 스타일 파일을 import 합니다
-
-const groups = [
-    { id: 1, name: 'Group One' },
-    { id: 2, name: 'Group Two' },
-    { id: 3, name: 'Group Three' },
-    { id: 4, name: 'Group Four' },
-];
+import axios from 'axios';
 
 const MyGroupPage = () => {
+    const [groups, setGroups] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchGroups = async () => {
+            try {
+                const user_no = localStorage.getItem('userNo');
+                const response = await axios.get(`http://localhost:5000/group/${user_no}`);
+                setGroups(response.data);
+                setLoading(false);
+            } catch (err) {
+                setError(err);
+                setLoading(false);
+            }
+        };
+
+        fetchGroups();
+    }, []); // 빈 배열을 의존성으로 전달하여 컴포넌트가 처음 렌더링될 때만 호출됨
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
     return (
         <div className="my-group-page">
             <header className="my-group-page-header">
@@ -17,16 +34,20 @@ const MyGroupPage = () => {
             </header>
             <main className="my-group-page-content">
                 <div className="group-list">
-                    {groups.map((group) => (
-                        <div key={group.id} className="group-card">
-                            <img
-                                src="/folder.png"
-                                alt={group.name}
-                                className="group-image"
-                            />
-                            <h3 className="group-name">{group.name}</h3>
-                        </div>
-                    ))}
+                    {groups.length === 0 ? (
+                        <p>No groups found</p>
+                    ) : (
+                        groups.map((group) => (
+                            <div key={group.group_no} className="group-card">
+                                <img
+                                    src="/folder.png"
+                                    alt={group.title}
+                                    className="group-image"
+                                />
+                                <h3 className="group-name">{group.title}</h3>
+                            </div>
+                        ))
+                    )}
                 </div>
             </main>
         </div>
