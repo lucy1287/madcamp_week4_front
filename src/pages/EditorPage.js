82294f34295b14930e9652b022d7d6445b42321d
lucy1287 <lucy1287@naver.com>
2024-07-25 16:08:30 +0900

    Feat: 글 배경색, 배경 이미지

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -27,7 +27,7 @@ const EditorPage = () => {
         setNoteColor(color);
     };
 
-    const handleBackgroundImageChange = (image) => {
+    const handleBackgroundImageChange = (image, index) => {
         setBackgroundImage(image);
     };
 
@@ -46,17 +46,37 @@ const EditorPage = () => {
 
     const handleSubmit = async () => {
         const user_no = localStorage.getItem("userNo");  // 실제 사용자의 번호로 변경해야 함
+        const jwtToken = localStorage.getItem('jwtToken'); // JWT 토큰 가져오기
+
+        const background_no = [
+            'https://i.pinimg.com/236x/dd/fa/b1/ddfab1e5d640d64d908a28460141455c.jpg',
+            'https://i.pinimg.com/236x/13/62/25/1362258d5fa4a20660ab5ede0ce9c0ed.jpg',
+            'https://i.pinimg.com/564x/b1/da/ea/b1daea970a878f2dc429e3ed32a6cb31.jpg',
+            'https://i.pinimg.com/236x/93/be/ce/93bece78e7528240f876e3b7483ce336.jpg',
+            'https://i.pinimg.com/474x/09/be/14/09be14bc67a40581bb45b751967dd5a0.jpg',
+            'https://i.pinimg.com/236x/75/9a/0e/759a0e7be2f7933a6009dedd298f23bb.jpg',
+            'https://i.pinimg.com/236x/31/95/01/31950148aee8feb7ac994331b9503f62.jpg',
+            'https://i.pinimg.com/236x/6b/90/87/6b90878a333b9bebc9271480bacdda6a.jpg',
+            'https://i.pinimg.com/236x/28/6e/9a/286e9ae0dd1b2c9d3c36d455ca8e4500.jpg',
+            'https://i.pinimg.com/236x/84/6b/1d/846b1d6c8284437ef3abe300ee6fd1a9.jpg',
+            'https://i.pinimg.com/236x/51/8e/08/518e08fd5b3d69371d3d5d7b2a7a2e52.jpg'
+        ].indexOf(backgroundImage) + 1; // 백그라운드 인덱스를 1부터 시작하도록 조정
+
+        const color_no = postItColors.indexOf(noteColor) + 1;
+        const font_no = fonts.indexOf(selectedFont) + 1;
+
         console.log(user_no);
         const response = await fetch(`http://localhost:5000/letter/create/${user_no}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwtToken}` // JWT 토큰을 헤더에 포함합니다.
             },
             body: JSON.stringify({
                 paper_no: paper_no,
                 content: noteText,
-                background_no: 3,  // 실제 데이터에 맞게 조정
-                color_no: postItColors.indexOf(noteColor) + 1,  // 색상 인덱스를 1부터 시작하도록 조정
+                background_no: background_no,  // 실제 데이터에 맞게 조정
+                color_no: color_no,  // 색상 인덱스를 1부터 시작하도록 조정
                 font_no: fonts.indexOf(selectedFont) + 1  // 폰트 인덱스를 1부터 시작하도록 조정
             }),
         });
@@ -149,7 +169,7 @@ const EditorPage = () => {
                                     src={image}
                                     alt={`Background ${index}`}
                                     className={backgroundImage === image ? 'selected' : ''}
-                                    onClick={() => handleBackgroundImageChange(image)}
+                                    onClick={() => handleBackgroundImageChange(image, index)}
                                 />
                             ))}
                         </div>
